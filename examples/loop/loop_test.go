7538a23f77c33d604af4e806d3d0f198503a5888
just-do-halee <just.do.halee@gmail.com>
2022-11-15 06:33:27 +0900

    Fix examples/loop

--- examples/loop/loop_test.go
+++ examples/loop/loop_test.go
@@ -11,13 +11,15 @@ func Sum(a, b int) int { return a + b }
 func TestSum(t *testing.T) {
 	type Args struct {
 		a, b int
+		// for distinguishing test cases
+		autoInc bool
 	}
 	type Ctx = *lum.Context[Args, int]
 
 	lum.Batch[Args, int]{
 		{
 			Name: "Increment lhs by 1, 100 times",
-			Args: Args{1, 1},
+			Args: Args{1, 1, true},
 			Pass: func(c Ctx) {
 				// assert
 				c.Logf("%v + %v = %v", c.Arguments.a, c.Arguments.b, c.Result)
@@ -25,11 +27,24 @@ func TestSum(t *testing.T) {
 			},
 			Loop: 100,
 		},
+		{
+			Name: "Increment rhs by 2, 10 times",
+			Args: Args{1, 1, false},
+			Pass: func(c Ctx) {
+				// assert
+				c.Logf("%v + %v = %v", c.Arguments.a, c.Arguments.b, c.Result)
+				c.AssertResultEqual(c.Arguments.a + c.Arguments.b)
+				c.Arguments.b += 2
+			},
+			Loop: 10,
+		},
 	}.Run(t, "Sum", func(a Args) int {
 		// before each
 		return Sum(a.a, a.b)
 	}, func(c Ctx) {
 		// after each
-		c.Arguments.a++
+		if c.Arguments.autoInc {
+			c.Arguments.a++
+		}
 	})
 }
